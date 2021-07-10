b62060a556b7908872b02733f20dee0be3b00e6a
Bruno_bR1cK5 <brsmc10@gmail.com>
2021-07-10 09:39:07 -0300

    Cadastro de Devs com Foto
    Cadastro de Devs com Foto

---
+++ CadastroDevsFoto/app/src/main/java/com/bruno/cadastrodevsfoto/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.bruno.cadastrodevsfoto
+
+enum class Senioridade (val nome: String) {
+    JUNIOR("Júnior"),
+    PLENO("Pleno"),
+    SENIOR("Sênior"),
+}
---
+++ CadastroDevsFoto/app/src/main/java/com/bruno/cadastrodevsfoto/Stack.kt
@@ -0,0 +1,8 @@
+package com.bruno.cadastrodevsfoto
+
+enum class Stack(val nome: String) {
+    FULLSTACK("Fullstack"),
+    BACKEND("Backend"),
+    FRONTEND("Frontend")
+
+}
